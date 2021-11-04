09ce355c777a1d1f4931321056f6e89357207d0f
Tom-Lewis-Coder <tomlewis139@gmail.com>
2021-11-04 14:29:10 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Header from './Components/Header'
+import Form from './Components/Form'
+import Button from './Components/Button'
+import WorkoutDisplay from './Components/WorkoutDisplay'
+import ExerciseList from './ExerciseList'
+import './Components/Styles.css'
 
 function App() {
+
+  const [ workout, setWorkout ] = useState([])
+  const [ difficulty, setDifficulty ] = useState('medium')
+
+  const getDifficulty = (e) => {
+    setDifficulty(e.target.value)
+  }
+
+  const getWorkout = () => { 
+    difficulty === 'low' ? setWorkout(ExerciseList[0].map(subArr => subArr[Math.floor(Math.random() * subArr.length)])) : 
+    difficulty === 'medium' ? setWorkout(ExerciseList[1].map(subArr => subArr[Math.floor(Math.random() * subArr.length)])) :
+    setWorkout(ExerciseList[2].map(subArr => subArr[Math.floor(Math.random() * subArr.length)]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' >
+      <Header title={'Random Workout Generator'} />
+      <Form title={'Choose difficulty and complete 3 times or change each time for variation. Rest 1 min between circuits'} onChange={getDifficulty} />
+      <Button onClick={getWorkout} title={'Get Workout'} />
+      <WorkoutDisplay workout={workout} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
