8852d8c031ec2a690c9a522180199d1ac8959ef0
Tom-Lewis-Coder <tomlewis139@gmail.com>
2021-11-05 11:55:04 +0000

    updated form format

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,25 +3,29 @@ import React from 'react'
 const Form = ( props ) => {
     return (
         <form className='form' >
-            <label className='form-title'>{props.title}</label><br/>
-                    <input 
-                        type='radio'
-                        name='radio'
-                        value='low' 
-                        onChange={props.onChange}
-                    />Low<br/>          
-                    <input 
-                        type='radio'
-                        name='radio' 
-                        value='medium' 
-                        onChange={props.onChange} 
-                    />Med<br/>                   
-                    <input 
-                        type='radio' 
-                        name='radio'
-                        value='high' 
-                        onChange={props.onChange} 
-                    />High<br/>   
+            <label className='form-title'>{props.title}</label><br/><br/>
+                <input 
+                    id='low'
+                    type='radio'
+                    name='radio'
+                    value='low' 
+                    onChange={props.onChange}
+                /><label className='radio' >Low</label><br/>                              
+                <input 
+                    id='medium'
+                    type='radio'
+                    name='radio' 
+                    value='medium' 
+                    onChange={props.onChange} 
+                /><label className='radio' >Med</label><br/>                   
+                <input
+                    id='high'                       
+                    type='radio' 
+                    name='radio'
+                    value='high' 
+                    onChange={props.onChange} 
+                /><label className='radio' >High</label> 
+            <p>Complete one circuit 3 times or change each time for a variation. Rest 1 min between circuits</p>
         </form>
         )}
 
