8bb29f91e5029921ccac46e4ce6a4cafd8bbff73
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-07-10 15:40:32 +0100

    customised radio buttons

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,28 +3,28 @@ import React from 'react'
 const Form = ( props ) => {
     return (
         <form className='form' >
-            <label className='form-title'>{props.title}</label><br/><br/>
+            <div className='form-title'>{props.title}</div><br/>
                 <input 
                     id='low'
                     type='radio'
                     name='radio'
                     value='low' 
                     onChange={props.onChange}
-                /><label className='radio' >Low</label><br/>                              
+                /><label className='radio' for='low' >Low</label>                              
                 <input 
                     id='medium'
                     type='radio'
                     name='radio' 
                     value='medium' 
                     onChange={props.onChange} 
-                /><label className='radio' >Med</label><br/>                   
+                /><label className='radio' for='medium' >Med</label>                   
                 <input
                     id='high'                       
                     type='radio' 
                     name='radio'
                     value='high' 
                     onChange={props.onChange} 
-                /><label className='radio' >High</label> 
+                /><label className='radio' for='high' >High</label> 
             <p>Complete one circuit 3 times or change each time for a variation. Rest 1 min between circuits</p>
         </form>
         )}
